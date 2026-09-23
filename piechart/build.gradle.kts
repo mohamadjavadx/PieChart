4dc42f98225c0a6f5c36b585a8d4f9a4c5382508
mohamadjavadx <mohamadjavad.pourmoradian@gmail.com>
2026-09-23 16:21:16 +0330

    Release 2.0.2

--- piechart/build.gradle.kts
+++ piechart/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 // Release details. Publishing to a remote repository refuses to run while any TODO/OWNER is left.
 // ---------------------------------------------------------------------------------------------
 group = "io.github.mohamadjavadx"
-version = "2.0.1"
+version = "2.0.2"
 
 val pomName = "PieChart"
 val pomDescription = "An animated pie / donut chart view for Android, with tap selection and " +
